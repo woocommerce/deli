2bb47fb38e0177bd1bf744ee90b4f38af048c182
James Koster <james@jameskoster.co.uk>
2015-03-31 15:49:17 +0100

    add header bg color option when using designer plugin. closes #4

--- inc/customizer/display.php
+++ inc/customizer/display.php
@@ -10,7 +10,7 @@
  * @return void
  */
 function storganic_add_customizer_css() {
-	$header_bg_color 			= storefront_sanitize_hex_color( get_theme_mod( 'storefront_header_background_color', apply_filters( 'storefront_default_header_background_color', '#fcfcfc' ) ) );
+	$navigation_bg_color 		= storefront_sanitize_hex_color( get_theme_mod( 'storefront_header_background_color', apply_filters( 'storefront_default_header_background_color', '#fcfcfc' ) ) );
 	$accent_color				= storefront_sanitize_hex_color( get_theme_mod( 'storefront_accent_color' ) );
 	$footer_link_color 			= storefront_sanitize_hex_color( get_theme_mod( 'storefront_footer_link_color', apply_filters( 'storefront_default_footer_link_color', '#96588a' ) ) );
 	$footer_heading_color 		= storefront_sanitize_hex_color( get_theme_mod( 'storefront_footer_heading_color', apply_filters( 'storefront_default_footer_heading_color', '#494c50' ) ) );
@@ -18,12 +18,13 @@ function storganic_add_customizer_css() {
 	$button_background_color 	= storefront_sanitize_hex_color( get_theme_mod( 'storefront_button_background_color', apply_filters( 'storefront_default_button_background_color', '#60646c' ) ) );
 	$button_text_color 			= storefront_sanitize_hex_color( get_theme_mod( 'storefront_button_text_color', apply_filters( 'storefront_default_button_text_color', '#ffffff' ) ) );
 
+	$header_bg_color 			= storefront_sanitize_hex_color( get_theme_mod( 'storganic_header_background_color', '' ) );
 
 	$darken_factor		= -15;
 	$lighten_factor		= 15;
 	$style = '
 		.storganic-primary-navigation {
-			background:' . $header_bg_color . ';
+			background:' . $navigation_bg_color . ';
 		}
 
 		.single-product div.product .summary .price {
@@ -46,6 +47,14 @@ function storganic_add_customizer_css() {
 		.header-widget-region h1, .header-widget-region h2, .header-widget-region h3, .header-widget-region h4, .header-widget-region h5, .header-widget-region h6 {
 			color: ' . $footer_heading_color . ';
 		}';
+
+	if ( class_exists( 'Storefront_Designer' ) ) {
+		$style .= '
+			.site-header {
+				background-color:' . $header_bg_color . ' !important;
+			}
+		';
+	}
 
 	wp_add_inline_style( 'storganic-style', $style );
 }
