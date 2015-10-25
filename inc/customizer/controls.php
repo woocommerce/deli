7ef493e89a546987d48fc846f12a6b8290267420
Bernhard <quasel@users.noreply.github.com>
2015-10-25 20:41:09 +0100

    Non Existent Customizer Section
    #8

--- inc/customizer/controls.php
+++ inc/customizer/controls.php
@@ -10,7 +10,7 @@
  * @return void
  */
 function deli_customize_storefront_controls( $wp_customize ) {
-	$wp_customize->get_control( 'storefront_header_background_color' )->section 	= 'nav';
+	$wp_customize->get_control( 'storefront_header_background_color' )->section 	= 'header_image';
 	$wp_customize->get_setting( 'storefront_header_background_color' )->transport 	= 'refresh';
 	$wp_customize->get_control( 'storefront_header_background_color' )->label 		= __( 'Navigation background color', 'deli' );
 
