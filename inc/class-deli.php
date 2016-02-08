81466bc9acc4d5f6461a02a68f3f08db009d10fa
Tiago Noronha <hello@tiagonoronha.com>
2016-02-08 16:55:07 +0000

    properly enqueue styles for 2.0 compatibility.

--- inc/class-deli.php
+++ inc/class-deli.php
@@ -20,6 +20,7 @@ class Deli {
 	 */
 	public function __construct() {
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_child_styles' ), 99 );
 		add_filter( 'storefront_custom_background_args', array( $this, 'background' ) );
 		add_action( 'storefront_loop_columns', array( $this, 'loop_columns' ) );
 		add_action( 'swc_product_columns_default', array( $this, 'loop_columns' ) );
@@ -32,18 +33,28 @@ class Deli {
 	 * @return void
 	 */
 	public function enqueue_styles() {
+		global $storefront_version;
+
+		wp_enqueue_style( 'storefront-style', get_template_directory_uri() . '/style.css', $storefront_version );
+	}
+
+	/**
+	 * Enqueue Storechild Styles
+	 * @return void
+	 */
+	public function enqueue_child_styles() {
 		global $storefront_version, $deli_version;
 
 		/**
 		 * Styles
 		 */
-		wp_enqueue_style( 'storefront-style', get_template_directory_uri() . '/style.css', $storefront_version );
 		wp_style_add_data( 'storefront-child-style', 'rtl', 'replace' );
 		wp_enqueue_style( 'copse', '//fonts.googleapis.com/css?family=Copse', array( 'storefront-child-style' ) );
 		wp_enqueue_style( 'anonymous-pro', '//fonts.googleapis.com/css?family=Anonymous+Pro:400,400italic,700', array( 'storefront-child-style' ) );
 		wp_enqueue_style( 'kalam', '//fonts.googleapis.com/css?family=Kalam:400,700', array( 'storefront-child-style' ) );
 		wp_enqueue_style( 'oswald', '//fonts.googleapis.com/css?family=Oswald', array( 'storefront-child-style' ) );
 	}
+
 
 	/**
 	 * Deli background settings
