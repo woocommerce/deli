b2dc3a509208c4afc99df6a68d77e2d4fee565b9
mikkamp <michael.kamp@automattic.com>
2020-05-20 14:37:10 +0100

    Build process for generating zip file

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,13 +46,81 @@ module.exports = function( grunt ) {
 					'style.css'
 				]
 			}
-		}
+		},
+
+		// Generate POT files.
+		makepot: {
+			options: {
+				type: 'wp-theme',
+				domainPath: 'languages',
+				potHeaders: {
+					'report-msgid-bugs-to': 'https://github.com/woocommerce/deli/issues',
+					'language-team': 'LANGUAGE <EMAIL@ADDRESS>'
+				}
+			},
+			frontend: {
+				options: {
+					potFilename: 'deli.pot',
+					exclude: [
+						'deli/.*' // Exclude deploy directory
+					]
+				}
+			}
+		},
+
+		// Creates deploy-able theme
+		copy: {
+			deploy: {
+				src: [
+					'**',
+					'.htaccess',
+					'!.*',
+					'!.*/**',
+					'!*.md',
+					'!*.scss',
+					'!.DS_Store',
+					'!assets/css/**/*.scss',
+					'!assets/css/sass/**',
+					'!assets/js/src/**',
+					'!composer.json',
+					'!composer.lock',
+					'!Gruntfile.js',
+					'!node_modules/**',
+					'!npm-debug.log',
+					'!package.json',
+					'!package-lock.json',
+					'!phpcs.xml',
+					'!deli/**',
+					'!deli.zip',
+					'!vendor/**'
+				],
+				dest: 'deli',
+				expand: true,
+				dot: true
+			}
+		},
+
+		compress: {
+			zip: {
+				options: {
+					archive: './deli.zip',
+					mode: 'zip'
+				},
+				files: [
+					{ src: './deli/**' }
+				]
+			}
+		}		
+
 	});
 
 	// Load NPM tasks to be used here
 	grunt.loadNpmTasks( 'grunt-sass' );
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
 	grunt.loadNpmTasks( 'grunt-rtlcss' );
+	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+	grunt.loadNpmTasks( 'grunt-contrib-copy' );
+	grunt.loadNpmTasks( 'grunt-contrib-compress' );
 
 	// Register tasks
 	grunt.registerTask( 'default', [
@@ -62,5 +130,16 @@ module.exports = function( grunt ) {
 	grunt.registerTask( 'css', [
 		'sass',
 		'rtlcss'
+	] );
+
+	grunt.registerTask( 'dev', [
+		'default',
+		'makepot'
+	] );
+
+	grunt.registerTask( 'deploy', [
+		'dev',
+		'copy',
+		'compress'
 	] );
 };
